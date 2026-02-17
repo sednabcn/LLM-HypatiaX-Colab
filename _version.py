0b7fec5928ed280348e7a52eebdc6336d1ddbddd
sednabcn <ruperto.bonet@modelphysmat.com>
2026-02-17 12:25:07 +0000

    Update requirements

--- _version.py
+++ _version.py
@@ -12,7 +12,8 @@ __all__ = [
 
 TYPE_CHECKING = False
 if TYPE_CHECKING:
-    from typing import Tuple, Union
+    from typing import Tuple
+    from typing import Union
 
     VERSION_TUPLE = Tuple[Union[int, str], ...]
     COMMIT_ID = Union[str, None]
@@ -27,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = "0.1.1.dev8+ge54293f1b.d20251108"
-__version_tuple__ = version_tuple = (0, 1, 1, "dev8", "ge54293f1b.d20251108")
+__version__ = version = '0.1.1.dev84+g3714c0fca.d20260217'
+__version_tuple__ = version_tuple = (0, 1, 1, 'dev84', 'g3714c0fca.d20260217')
 
-__commit_id__ = commit_id = "ge54293f1b"
+__commit_id__ = commit_id = 'g3714c0fca'
